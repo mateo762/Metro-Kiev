95852188ecfcab3d0e5259376c1047ecd2b0d7bc
mate762 <mateo.echeverryhoyos@gmail.com>
2021-12-09 16:59:01 +0100

    ready to deploy

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log("Listening on port 3000");
+    console.log(`Listening on port ${PORT}`);
 })
